1040e118e87e4234768c26a5ddcd6749b23e0ae6
mario_l <mariobillie459@gmail.com>
2026-06-13 10:13:58 -0400

    pegado

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Header from "../components/Header/Header";
-import ExerciseCard from "../components/ExerciseCard/ExerciseCard";
-import ExerciseList from "@/components/ExerciseList/ExerciseList";
+
+import ExerciseList from "../components/ExerciseList/ExerciseList";
 export default function Home() {
   return (
     <main className = "container  ">
